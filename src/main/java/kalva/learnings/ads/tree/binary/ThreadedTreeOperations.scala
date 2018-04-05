0f9b08fb607051b7b129cdb6d299fe6e97331363
sunilkumar kalva <sunilkumar.kalva@SW-LP00250.local>
2018-04-05 18:24:32 +0530

    ThreadedNode

--- src/main/java/kalva/learnings/ads/tree/binary/ThreadedTreeOperations.scala
+++ src/main/java/kalva/learnings/ads/tree/binary/ThreadedTreeOperations.scala
@@ -1,20 +1,23 @@
 package kalva.learnings.ads.tree.binary
 
 import kalva.learnings.ads.tree.ThreadedNode
-import kalva.learnings.ads.tree.ThreadedNode.createSampleTree
+import kalva.learnings.ads.tree.ThreadedNode.{bottomRightNull, createSampleTree, findInorderSuccessor, printInOrder}
 
+/**
+  * https://www.geeksforgeeks.org/threaded-binary-tree/
+  */
 object ThreadedTreeOperations extends App {
 
   populateRightNull(createSampleTree())
 
   def populateRightNull(root: ThreadedNode): Unit = {
-    val root = ThreadedNode.createSampleTree()
-    val nodes = ThreadedNode.bottomRightNull(root)
-    ThreadedNode.printInOrder(root)
+    val root = createSampleTree()
+    val nodes = bottomRightNull(root)
+    printInOrder(root)
     for (node: ThreadedNode <- nodes) {
-      ThreadedNode.findInorderSuccessor(root, node.data)
+      findInorderSuccessor(root, node.data)
     }
     println()
-    ThreadedNode.printInOrder(root)
+    printInOrder(root)
   }
 }
